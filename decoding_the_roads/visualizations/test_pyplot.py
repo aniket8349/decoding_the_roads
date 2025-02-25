0b5b11156996f2a87bb2334a5c030bb2d25c0425
Aniket sahani <aniketsahani.as@gmail.com>
2025-02-25 22:07:04 +0530

    added few chart utlity function

--- decoding_the_roads/visualizations/test_pyplot.py
+++ decoding_the_roads/visualizations/test_pyplot.py
@@ -1,6 +1,23 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from plotly import express as px
+from plotly.graph_objs import Figure
+from pandas import DataFrame
 
-fig, ax = plt.subplots()  
-ax.plot([1, 2, 3, 4], [1, 4, 2, 3]) 
-plt.show() 
+# line chart utility function
+def line_chart(data , x: str , y: str, title: str, )-> Figure:
+    df: DataFrame = DataFrame(data)
+    fig: Figure = px.line(df, x=x, y=y, title=title)
+    return fig
+
+def bar_chart(data , x: str , y: str, title: str, )-> Figure:
+    df: DataFrame = DataFrame(data)
+    fig: Figure = px.bar(df, x=x, y=y, title=title)
+    return fig
+
+
+
+if __name__ == "__main__":
+    fig = line_chart(data={'x': [1, 2, 3, 4, 5], 'y': [2, 4, 1, 5, 3]}, x='x', y='y', title='Sample Plotly Chart')
+    fig.write_image("./output/line_chart.png")
+    #print(fig)
+
+
