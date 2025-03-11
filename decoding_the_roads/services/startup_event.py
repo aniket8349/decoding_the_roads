aeaf346df93eff67346d5fc4da23a85feaf6cd4e
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2025-03-11 17:06:03 +0530

    fix: kaggel import error

--- decoding_the_roads/services/startup_event.py
+++ decoding_the_roads/services/startup_event.py
@@ -3,7 +3,6 @@ import os
 from ..config.db_config import db_config
 from ..utils.csv_to_mysql import csv_to_mysql
 from ..utils.get_column_names import get_column_names
-from ..config.download_data import download_data
 from ..constant.constant import TABLE_NAME, DATA_DIR
 from ..utils.sql_utils import fetch_one
 from ..utils.logger import setup_logger
@@ -23,8 +22,11 @@ async def startup_event():
         check_databases = fetch_one(db_config, f"SHOW DATABASES LIKE '{db_config['database']}'")
      
         if check_databases:
+            
             logger.info("Startup complete.")
         else:
+            from ..config.download_data import download_data
+
             download_data(dataset_slug, DATA_DIR)  # Pass kaggle_api to download function
 
             column_list = get_column_names(csv_file)
@@ -32,9 +34,7 @@ async def startup_event():
                 csv_to_mysql(db_config, csv_file, TABLE_NAME, column_list)
             else:
                 raise RuntimeError("Failed to get column names from CSV file.")
-            
-
-        logger.info("Startup complete.")
+            logger.info("Startup complete.")
 
     except Exception as e:  # Catch potential startup errors
         logger.error(f"Startup error: {e}")
