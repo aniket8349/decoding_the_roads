acc040a786d4c0e9a5af9115c7157384ed0d4e0f
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2025-03-11 16:56:33 +0530

    fix: kaggel error

--- decoding_the_roads/services/startup_event.py
+++ decoding_the_roads/services/startup_event.py
@@ -5,7 +5,7 @@ from ..utils.csv_to_mysql import csv_to_mysql
 from ..utils.get_column_names import get_column_names
 from ..config.download_data import download_data
 from ..constant.constant import TABLE_NAME, DATA_DIR
-
+from ..utils.sql_utils import fetch_one
 from ..utils.logger import setup_logger
 
 logger = setup_logger(__name__)  
@@ -19,13 +19,17 @@ async def startup_event():
         csv_file: str = os.path.join(DATA_DIR, "global_traffic_accidents.csv")
         dataset_slug: str = os.getenv("KAGGLE_DATASET_SLUG")
 
-        download_data(dataset_slug, DATA_DIR)  # Pass kaggle_api to download function
+        # check if database does not exist
+        check_databases = fetch_one(db_config, f"SHOW DATABASES LIKE '{db_config['database']}'")
+        if not check_databases:
+            
+            download_data(dataset_slug, DATA_DIR)  # Pass kaggle_api to download function
 
-        column_list = get_column_names(csv_file)
-        if column_list:
-            csv_to_mysql(db_config, csv_file, TABLE_NAME, column_list)
-        else:
-            raise RuntimeError("Failed to get column names from CSV file.")
+            column_list = get_column_names(csv_file)
+            if column_list:
+                csv_to_mysql(db_config, csv_file, TABLE_NAME, column_list)
+            else:
+                raise RuntimeError("Failed to get column names from CSV file.")
 
         logger.info("Startup complete.")
 
